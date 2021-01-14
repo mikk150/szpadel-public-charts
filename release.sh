c065209788b7eec43d1fd1531a84d13e260412aa
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2021-01-14 16:41:05 +0100

    Initial commit

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# != 1 ];then
+  echo "Usage: $0 <chart name>"
+  exit 1
+fi
+
+helm3 package charts/$1 -d releases/
+
+helm3 repo index --merge index.yaml
